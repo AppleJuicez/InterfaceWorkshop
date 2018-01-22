e1b34fd54a90136117bc264db44597fc62ee7c6c
AJZ <jinnawat202@gmail.com>
2018-01-22 13:24:08 +0700

    Finished modifying 6 months into savings

---
+++ src/DepositSaving.java
@@ -0,0 +1,52 @@
+public class DepositSaving  implements Tenurable, Profitable {
+
+	public double interestRate;
+	public double capital;
+	public double month_invested;
+	public double month_tenured;
+	
+	public DepositSaving(double capital , double month_invested) 
+	{
+		this.interestRate = 0.25;
+		this.month_tenured = 1;
+		this.capital = capital;
+		this.month_invested = month_invested;
+	}
+	
+	@Override
+	public double getInterest() 
+	{
+		return this.interestRate;
+	}
+
+	@Override
+	public double calculateProfit(double capital, double month) 
+	{
+		double profit = capital * ( (int)month / this.month_tenured * this.interestRate);
+		return profit;
+	}
+
+	@Override
+	public double calculateTotal(double capital, double month) 
+	{
+		double profit = capital * (100.0 + ( (int)month / this.month_tenured * this.interestRate ));
+		return profit;
+	}
+
+	@Override
+	public String printCondition() {
+		return "Profit is " + interestRate + "% per " + month_tenured + " months";
+	}
+
+	@Override
+	public double getConditionPeriod() {
+		return month_tenured;
+	}
+
+	@Override
+	public boolean isTenured(double month) {
+		// TODO Auto-generated method stub
+		return ((int)month/(int)month_tenured >0);
+	}
+
+}
