50cedeb096d694dc50bdf0a35604ebadd9a13ba2
AJZ <jinnawat202@gmail.com>
2018-01-22 13:01:33 +0700

    finished Profitable

--- src/Profitable.java
+++ src/Profitable.java
@@ -4,4 +4,5 @@ public interface Profitable
 	double getInterest();
 	double calculateProfit(double capital , int month);
 	double calculateTotal(double capital, int month);
+	String printCondition();
 }
